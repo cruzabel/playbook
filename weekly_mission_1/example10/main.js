fd541535c8b86cf076e4496d079a0d5b123c493d
Abel De La Cruz Martinez <blueneon@outlook.es>
2022-04-10 22:14:24 -0500

    add  Example 10: Ejercicio

---
+++ weekly_mission_1/example10/main.js
@@ -0,0 +1,7 @@
+import MyPokemon from './pokemon.js'
+
+// const pikachu = new MyPokemon('Pikachu')
+// pikachu.sayHello()
+
+const charmander = new MyPokemon('Charmander :)')
+charmander.sayHello()
