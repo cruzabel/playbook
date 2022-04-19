447cd78e27c8c335bd3482405c80ec2259f5f70b
Abel De La Cruz Martinez <blueneon@outlook.es>
2022-04-19 13:55:31 -0500

    add exmple3

---
+++ weekly_mission_2/examples/1objetos/example3.js
@@ -0,0 +1,12 @@
+// Ejemplo 3: Objeto con diferentes propiedades
+const myFriend = {
+    name: "Guayabo",
+    age: 1,
+    nicknames: ['guayas','gato'],
+    address: {city:'mexico'}
+    
+}
+console.log("Ejemplo 3: Objeto con diferentes propiedades")
+console.log(myFriend)
+console.log(myFriend.name)
+console.log(myFriend["address"])
